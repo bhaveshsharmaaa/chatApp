c6187b09a32ae049771c32decd14c65f42bbc849
bhaveshsharmaaa <bhaveshsharma7456@gmail.com>
2025-05-03 16:46:01 +0530

    sved change

--- server/src/index.js
+++ server/src/index.js
@@ -8,11 +8,14 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import { app, server } from "./lib/socket.js";
 import path from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config();
 
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
+
+console.log("cccccc", __dirname);
 
 // Important: increase payload limits
 app.use(express.json({ limit: "10mb" }));
@@ -44,14 +47,6 @@ app.use("/api/messages", messageRoutes);
 
 console.log("chat routes registered");
 app.use("/api/chat", tokenWalaRoutes);
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
 
 // Server
 server.listen(PORT, () => {
