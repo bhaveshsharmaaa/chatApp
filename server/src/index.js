32a786c5d19ca0c3af3bc29233dc3aafd4d4bae8
bhaveshsharmaaa <bhaveshsharma7456@gmail.com>
2025-05-03 16:53:39 +0530

    sved change

--- server/src/index.js
+++ server/src/index.js
@@ -18,25 +18,16 @@ const __dirname = path.resolve();
 console.log("cccccc", __dirname);
 
 // Important: increase payload limits
-app.use(express.json({ limit: "10mb" }));
-app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+app.use(express.json());
+app.use(cookieParser());
 
 // CORS setup
 app.use(
   cors({
     origin: "http://localhost:5173",
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
-
-// Important: manually allow credentials
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Credentials", "true");
-  next();
-});
-
-app.use(cookieParser());
 
 // Your Routes
 console.log("Registering /api/auth");
@@ -47,6 +38,14 @@ app.use("/api/messages", messageRoutes);
 
 console.log("chat routes registered");
 app.use("/api/chat", tokenWalaRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 // Server
 server.listen(PORT, () => {
