4b25e0d5d97ba45ad6d76fbc9307b46c6bdce2ba
bhaveshsharmaaa <bhaveshsharma7456@gmail.com>
2025-05-03 18:17:15 +0530

    sved change

--- server/src/index.js
+++ server/src/index.js
@@ -23,7 +23,7 @@ app.use(cookieParser());
 // CORS setup
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://chatapp-xan2.onrender.com",
     credentials: true,
   })
 );
